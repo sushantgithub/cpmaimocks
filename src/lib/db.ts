99598edffc7da6dc0d5f76cfbf571828cd7ec6b4
sushantgithub <kulk.sushant@gmail.com>
2026-09-21 17:29:50 +0530

    Guard staging database and payment configuration

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,7 @@
 import { PrismaClient } from '@prisma/client'
+import { assertSafeStagingEnvironment } from '@/lib/environment-safety'
+
+assertSafeStagingEnvironment()
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
